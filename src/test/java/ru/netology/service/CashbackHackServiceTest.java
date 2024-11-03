52d616132c1438ac9bf789bca2ba24dc487483f0
Evgeniya Malashenko <malashenko_genya@mail.ru>
2024-11-03 17:06:02 +0300

    Adding tests and dependencies to junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CashbackHackServiceTest {
     @Test // не хватает суммы для кешбека
@@ -11,6 +9,14 @@ public class CashbackHackServiceTest {
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(999);
         int expected = 1;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test // кешбек начисляется при покупке в 1000 р.
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(1000);
+        int expected = 0;
         Assert.assertEquals(actual, expected);
     }
 }
